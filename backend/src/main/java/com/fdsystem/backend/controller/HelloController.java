bb629c6182d7811493637e3c7db99bf71903ccb7
shaik mohammad <shaikmohammad@TBMDVSHIKMOH.local>
2025-09-19 14:49:21 +0530

    added essential packages

--- backend/src/main/java/com/fdsystem/backend/controller/HelloController.java
+++ backend/src/main/java/com/fdsystem/backend/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.sarthakkurothe.backend.controller;
+package com.fdsystem.backend.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
--- backend/src/test/java/com/fdsystem/backend/DigitalBackendApplicationTests.java
+++ backend/src/test/java/com/fdsystem/backend/DigitalBackendApplicationTests.java
@@ -1,4 +1,4 @@
-package com.sarthakkurothe.backend;
+package com.fdsystem.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
